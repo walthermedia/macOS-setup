1ea83a55da6051da4ee4164e9b7b3be4b72956aa
Ben <119703246+walthermedia@users.noreply.github.com>
2023-10-23 21:26:54 -0500

    Add files via upload

---
+++ dotfiles/exports.zsh
@@ -0,0 +1,17 @@
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Oh my Zsh framework
+export ZSH=~/.oh-my-zsh
+
+# Term Colors
+export TERM=xterm-256color
+
+# Binutils
+export PATH="/usr/local/opt/binutils/bin:$PATH"
+
+# curl auto complete
+export FPATH="/usr/local/opt/curl/share/zsh/site-functions:$FPATH"
+
+# opt out telemetry
+export DO_NOT_TRACK=1
